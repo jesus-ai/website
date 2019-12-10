7e4f8ea35cf8f037c88b770dc6fea5e83e823e83
duncte123 <duncan.sterken123@gmail.com>
2019-12-10 15:16:40 +0100

    Add /api?xml

--- pages/api.php
+++ pages/api.php
@@ -3,6 +3,15 @@ header('Content-Type: application/json', true);
 
 $db = new PDO('mysql:host='.$db['host'].';dbname='.$db['db'], $db['user'], $db['pass']);
 $query = $db->query('SELECT SayingText as text, SayingDate as date FROM sayings ORDER BY RAND() LIMIT 1');
-$res = $query->fetchAll(PDO::FETCH_OBJ);
+$res = $query->fetch(PDO::FETCH_ASSOC);
 
-echo json_encode($res);
+if (isset($_GET['xml'])) {
+    header('Content-Type: text/xml', true);
+
+    $xml = new SimpleXMLElement('<say-ai/>');
+    $saying = $xml->addChild('saying');
+    array_walk_recursive(array_flip($res), [$saying, 'addChild']);
+    die($xml->asXML());
+}
+
+die(json_encode($res));
