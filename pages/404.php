a00e8cb3547f642408eabc936af3f65eed573d73
Jeremey van Uden <tasje34@gmail.com>
2019-12-13 09:36:07 +0100

    added 404 page

---
+++ pages/404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<div id="particles-js"></div>
+<div class="resize"></div>
+<div class="container">
+    <div class="form_container">
+        <div class="form animated bounceInDown">
+            <div class="error_container"><h2>404 Page Not Found</h2></div>
+        </div>
+    </div>
+</div>
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
+<script src="js/main.js"></script>
+
+<template id="content-template">
+
+</template>
+</body>
+</html>
